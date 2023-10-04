8ebe3ab5840075617bbefe1b975419ba7c75b2d6
EwerthonMelzani <ervithon@gmail.com>
2023-10-04 15:22:28 +0200

    Frances

--- English/automatization_english.sh
+++ English/automatization_english.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
-for fichiers in $(ls)
+for fichiers in *
 do
-    if [ -f "$fichiers" ];then
+    if [ -f "$fichiers" ]; then
 
-        if [ "$fichiers" =~ ^Classe_[1-9]_lesson ];then
+        if [[ "$fichiers" =~ ^(Classe_[1-9]_lesson) ]]; then
 
-            arquivo="$fichiers"
+            # Extract the matched part of the filename
+            directory_name="${BASH_REMATCH[1]}"
 
-            mkdir ""
+            # Create the directory if it doesn't already exist
+            if [ ! -d "$directory_name" ]; then
+                mkdir "$directory_name"
+                echo "Created directory: $directory_name"
+            fi
 
+            # Move the file to the directory
+            mv "$fichiers" "$directory_name/"
+            echo "Moved $fichiers to directory $directory_name"
         fi
 
-    fi
+        if [[ "$fichiers" =~ ^(Classe_[1-9]_lesson\..*) ]]; then
 
+            # Extract the matched part of the filename
+            directory_name="${BASH_REMATCH[1]}"
+
+            # Create the directory if it doesn't already exist
+            if [ ! -d "$directory_name" ]; then
+                mkdir "$directory_name"
+                echo "Created directory: $directory_name"
+            fi
+
+            # Move the file to the directory
+            mv "$fichiers" "$directory_name/"
+            echo "Moved $fichiers to directory $directory_name"
+        fi
+    fi
 done
